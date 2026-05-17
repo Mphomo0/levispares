32cffc58e17666c5c35eb16383a9a98f514a6ebc
Mphomo0 <mphomoipolai1@gmail.com>
2026-05-17 19:25:47 +0200

    chore: add dynamic Clerk domain based on deployment type

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,3 +1,10 @@
-export default {
-  providers: [],
-};
+export default ({ deploymentType }) => ({
+  providers: [
+    {
+      domain: deploymentType === "production"
+        ? "https://clerk.levispares.co.za/"
+        : "https://talented-dane-13.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+});
